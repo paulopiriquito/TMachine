9367551a43412df4ac0bbec368f004045e9a2534
joaof96 <joao_f_96@hotmail.com>
2016-05-02 20:00:40 +0100

    Ticket Printer Stations array

--- src/Stations.java
+++ src/Stations.java
@@ -2,4 +2,8 @@
  * Created by a3908 on 11/03/2016.
  */
 public class Stations {
+    private static String[] stations = {"Cascais","Estoril","S. João","S.Pedro","Parede","Carcavelos","Oeiras",
+                                        "Santo Amaro", "Paço de Arcos", "Caxias","Cruz-Quebrada","Algés","Belém",
+                                        "Alcântara-Mar", "Santos", "Cais do Sodré"};
+
 }
--- src/TicketPrinter.java
+++ src/TicketPrinter.java
@@ -2,4 +2,21 @@
  * Created by a3908 on 11/03/2016.
  */
 public class TicketPrinter {
+    /**
+     * Inicia a classe, estabelecendo os valores iniciais.
+     */
+    public static void init(){
+        SerialEmitter.init();
+    }
+
+    /**
+     * Envia comando para imprimir e dispensar um bilhete para a estação toId, roundTrip a true se
+     * o bilhete for de ida/volta
+     */
+    public static void print(int toId, boolean roundTrip){
+        toId <<= 1;
+        if (roundTrip)
+            toId |= 1;
+        SerialEmitter.send(false, toId);
+    }
 }

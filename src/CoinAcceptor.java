5f3964597a2a7f96aa8e4652493d963f382f4798
Paulo <pp.10.b.ct@gmail.com>
2016-06-09 06:58:15 +0100

    Coin Deposit e Coin Acceptor

--- src/CoinAcceptor.java
+++ src/CoinAcceptor.java
@@ -2,5 +2,24 @@
  * Created by a3908 on 11/03/2016.
  */
 public class CoinAcceptor {
+    //Inicia a classe
+    public static void init(){
 
+    }
+    //Retorna !=0 se foi introduzida uma nova moeda, restantes valores identificam o tipo
+    public static int hasCoin(){
+
+    }
+    //Informa o moedeiro que a moeda foi contabilizada.
+    public static void acceptCoin(){
+
+    }
+    //Devolve as moedas que estão no moedeiro.
+    public static void ejectCoins(){
+
+    }
+    //Recolhe as moedas que estão no moedeiro.
+    public static void collectCoins(){
+
+    }
 }
--- src/CoinDeposit.java
+++ src/CoinDeposit.java
@@ -2,4 +2,22 @@
  * Created by a3908 on 11/03/2016.
  */
 public class CoinDeposit {
+    private static int coinCounter;
+    private static final double CTYPE = 0.5;
+
+    public static void reset(){
+        coinCounter = 0;
+    }
+
+    public static double getValue(){
+        return coinCounter * CTYPE;
+    }
+
+    public static void addCoin(){
+        ++coinCounter;
+    }
+
+    public static void removeCoin(){
+        --coinCounter;
+    }
 }
